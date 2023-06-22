6394c760472ea5d5faaed084d629770cdb2000d2
ChamikaRathnayaka <chamikarath97@gmail.com>
2023-06-22 14:59:59 +0530

    individual products created

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PrroductCard from './PrroductCard'
+import jsonData from '../api/data.json';
 
 export default function Product() {
+
   return (
     <div className='py-10'>
         <div className='flex flex-col items-center gap-4'>
@@ -14,7 +16,32 @@ export default function Product() {
                 molestiae vitae nemo beatae soluta fugiat laborum facere repudiandae amet consectetur.</p>
         </div>
 
-        <PrroductCard/>
+      {/* individual products from json */}
+        <div className=' max-w screen-xl mx-auto py-10 grid grid-cols-4 gap-10 ml-10 mr-10'>
+        {jsonData.products.map((product) => (
+          <div className='group'>
+
+            <div key={product._id} className='w-full h-96 cursor-pointer overflow-hidden '>
+              <img className='w-full h-full  object-cover group-hover:scale-110 duration-500 ' src={product.image} alt={product.title} />
+           </div>
+
+           <div className='flex justify-between items-center w-full border-[1px] px-2 py-4  text-base font-bold'>
+              <div>
+                <h2>{product.title}</h2>
+              </div>
+              <div className='flex gap-2'>
+                    <p className='line-through text-gray-500'>${product.oldPrice}</p>
+                    <p className='font-semibold'>${product.price}</p>
+              </div>
+              
+           </div>
+
+          </div>
+        ))}
+      </div>
+
+       
     </div>
   )
 } 
+
